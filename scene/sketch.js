2dce9c3020c5f2f4aec8cd10c9eccbfdd7012b80
Allhush <hushagenalliam@gmail.com>
2024-03-11 19:34:02 -0600

    created background, added comments

--- scene/sketch.js
+++ scene/sketch.js
@@ -5,6 +5,7 @@ let state = "start screen";
 let lost = 0;
 let state2 = "up";
 let counter = 0;
+let bGroundS = "off";
 let q;
 let g;
 let weapon = "bang bang";
@@ -22,7 +23,7 @@ let rE2 = 20000;
 let hb;
 let rex;
 let rey;
-let rS = "not spawning";
+let rS = "else";
 let dya;
 // let ax;
 // let bx;
@@ -41,11 +42,12 @@ let dya;
 function setup() {
   noCursor();
   varTwo = 1;
-  rE = random(20000, 30000);
+  rE = random(5000, 10000);
 }
 
 function draw() {
   createCanvas(windowWidth, windowHeight);
+  bGround();
   healthBar();
   startScreen();
   clayPigeon();
@@ -53,11 +55,12 @@ function draw() {
   speedUp();
   crossHair();
   youDead();
-  randomEvent();
+  // randomEvent();
   // deadEnemies();
   // knifeTime();
 }
 
+// makes crosshair to replace mouse
 function crossHair(){
   if (weapon === "bang bang"){
     // fill(0);
@@ -76,12 +79,15 @@ function crossHair(){
 }
 
 function clayPigeon(){
+  // used for health bar
   q = lost;
+  // creates spawning position for clay pigeon
   if (state === "spawning"){ 
     y = windowHeight;
     x = random(windowWidth/4, 3*windowWidth/4);
     state = "not spawning";
   }
+  // movement for clay pigeon
   else if (state === "not spawning"){
     g = random(height/2, height/6);
     if(y >= g && state2 === "up"){
@@ -101,26 +107,34 @@ function clayPigeon(){
       state2 = "up";
       q += 1;
     }
+    // creates hitbox for clay pigeon 
     if (mouseX > x - 10 && mouseX < x + 10 && mouseY > y - 10 && mouseY < y + 10 && mouseIsPressed){
       counter += 1;
       state = "spawning";
       state2 = "up";
     }
   }
+  // creates clay pigeon
   circle(x,y,20);
 }
 
+
 function healthBar(){
+  // checks health
   if (lost < 11){
+    // creates solid health bar
     for(let u = width/11; u <= width; u += width/12){
       fill(0);
       rect(u, 25, 35, 20);
     }
     fill(220);
+    // fills health bar
     rect(width/11, 25, 35 + lost*width/12, 20);
     
   }
+  // checks health
   else if (lost > 10 && lost < 20){
+    // kills player
     state = "dead";
     varTwo = 1;
     p = millis();
@@ -130,10 +144,17 @@ function healthBar(){
 
 function youDead(){
   if (state === "dead"){
+    // displays death message
     fill(0);
     textAlign(CENTER);
     textSize(50);
     text("You died", width/2, height/2);
+    // textAlign(CENTER);
+    // textSize(20);
+    // text("press to play again", width/2, height/2 + height/10);
+    // if(mouseIsPressed && mouseY > height/2 + height/10 && mouseY < height/2 + height/5 && mouseX > 3*width/4 && mouseX < width/4){
+    //   state = "spawning";
+    // }
   }
   // if (state === "dead" && millis()> p + 2000){
   //   state = "deademies";
@@ -173,6 +194,8 @@ function youDead(){
 //   circle(fx, fy, 20);
 // }
 
+
+// keeps score
 function scorekeep(){
   fill(0);
   textSize(50);
@@ -180,57 +203,73 @@ function scorekeep(){
   text("You have " + counter + " points!", 10, height - 60);
 }
 
+// creates start screen
 function startScreen(){
   if (state === !"start screen"){
     background(220);
   }
   if (state === "start screen" && varTwo === 1){
+    // displays start screen
     background(0);
     fill(0);
     rect(width/11, 25, 35, 20);
     textAlign(CENTER, CENTER);
     fill(255);
     text("Press the mouse to start", width/2, height/2);
+    // starts game
     if (mouseIsPressed){
       background(220);
       state = "spawning";
+      bGroundS = "on";
       varTwo = 0;
     }
   }
 }
 
 function speedUp(){
+  // speeds up clay pigeon
   if (counter > 10 && millis() > aw + 20000){
     dxa += 1;
     aw = millis();
   }
 }
 
-function randomEvent(){
-  if (millis() > rE + rE2){
-    rS = "spawning";
-  }
-  if (rS === "spawning"){
-    rex = windowWidth;
-    rey = random(3*windowHeight/4, windowHeight/4);
-    rS = 1;
-    rE = millis();
-    rE2 = random(2000, 50000);
-  }
-  if (rS === 1){
-    rex -= 10;
-    rey += dya;
-    if (rex < width/2){
-      dya = -dya;
-    }
-    dya = -dya;
-  }
-  if (mouseX > rex - 10 && mouseX < rex + 10 && mouseY > rey - 10 && mouseY < rey + 10){
-    counter += 7;
-    rS = "else";
-  }
-  circle(rex, rey, 20);
+function bGround(){
+  if (bGroundS === "on")
+  noStroke();
+  fill("green");
+  rect(0, 2*height/3, width, height/3);
+  fill("skyblue");
+  rect(0, 0, width, 2*height/3);
+  stroke(0);
 }
+
+// function randomEvent(){
+//   if (millis() > rE + rE2 && rS === "else"){
+//     rS = "spawning";
+//   }
+//   if (rS === "spawning"){
+//     rex = windowWidth - 10;
+//     rey = random(height/4, 3*height/4);
+//     rS = 1;
+//     rE = millis();
+//     rE2 = random(2000, 10000);
+//   }
+//   if (rS === 1){
+//     rex -= 10;
+//     rey += dya;
+//     if (rex > width/2){
+//       dya = -dya;
+//     }
+//   }
+//   if (mouseX > rex - 10 && mouseX < rex + 10 && mouseY > rey - 10 && mouseY < rey + 10){
+//     counter += 7;
+//     rS = "else";
+//     dya = -dya;
+//   }
+//   fill(0);
+//   circle(rex, rey, 20);
+// }
 
 // function knifeTime(){
 //   if (counter%10 === 0 && counter > 1){
